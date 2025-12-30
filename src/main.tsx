c3fb4713df68f1167293ea365c007838bccfb0eb
frostwebdev-dotcom <frostwebdev@gmail.com>
2025-12-30 10:26:32 -0500

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -4,50 +4,6 @@ import "./index.css";
 import { HelmetProvider } from "react-helmet-async";
 import { logIntegrationStatus } from "./utils/integrationCheck";
 import { Capacitor } from "@capacitor/core";
-import logoIcon from "@/assets/digsandgigs-logo-icon.svg";
-
-// Set favicon dynamically from assets
-const setFavicon = (iconPath: string) => {
-  const isSvg = iconPath.endsWith('.svg');
-  const isIco = iconPath.endsWith('.ico');
-  
-  const link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
-  if (link) {
-    link.href = iconPath;
-    if (isSvg) {
-      link.type = "image/svg+xml";
-    } else if (isIco) {
-      link.type = "image/x-icon";
-    }
-  } else {
-    const newLink = document.createElement("link");
-    newLink.rel = "icon";
-    if (isSvg) {
-      newLink.type = "image/svg+xml";
-    } else if (isIco) {
-      newLink.type = "image/x-icon";
-    } else {
-      newLink.type = "image/png";
-    }
-    newLink.href = iconPath;
-    document.getElementsByTagName("head")[0].appendChild(newLink);
-  }
-  
-  // Also set apple-touch-icon (use the same icon)
-  const appleLink = document.querySelector("link[rel~='apple-touch-icon']") as HTMLLinkElement;
-  if (appleLink) {
-    appleLink.href = iconPath;
-  } else {
-    const newAppleLink = document.createElement("link");
-    newAppleLink.rel = "apple-touch-icon";
-    newAppleLink.sizes = "180x180";
-    newAppleLink.href = iconPath;
-    document.getElementsByTagName("head")[0].appendChild(newAppleLink);
-  }
-};
-
-// Set favicon on app load
-setFavicon(logoIcon);
 
 // Initialize Capacitor for mobile platforms
 if (Capacitor.isNativePlatform()) {
