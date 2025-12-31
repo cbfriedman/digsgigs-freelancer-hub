6728f412d490e1bc95c0f07a3b0d6876eeaa8ac5
frostwebdev-dotcom <frostwebdev@gmail.com>
2025-12-30 20:21:57 -0500

    commited

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import logo from "@/assets/digsandgigs-logo.svg";
+import logo from "@/assets/footer-logo.svg";
 
 export const Footer = () => {
   const navigate = useNavigate();
